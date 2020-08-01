f8e24fa152c203accba3ece24ac09aff235fc1a8
Dan Rosen <drosen@precipart.com>
2020-08-01 14:48:44 -0400

    Initial commit

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject game = GameObject.Find("Ball");
+        Ball ball = game.GetComponent<Ball>();
+
+        if (ball.hasStarted)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# unity-block-breaker
+ Block Breaker game in Unity, practice project via GameDev.tv on Udemy
